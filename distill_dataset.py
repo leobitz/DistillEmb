030c55d9a215377134a1184500ccf8e413e8cd7c
Amanuel Negash <amanyamy@gmail.com>
2022-10-07 23:27:18 +0300

    testing works

--- distill_dataset.py
+++ distill_dataset.py
@@ -120,7 +120,7 @@ def collate_fun(batch):
         batch_mask_idx.append(_mask_id)
     
     batch_mask_idx = torch.LongTensor(batch_mask_idx)
-    # batch_words = torch.stack(batch_words)
+    batch_words = torch.stack(batch_words)
     batch_labels = torch.LongTensor(batch_labels)
 
     return batch_words, batch_labels, batch_mask_idx
